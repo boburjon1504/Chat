86c9499d02511c050a1cc6a3b79c03d5d116764b
boburjon1504 <boburjon67joraboyev@gmail.com>
2024-04-29 09:27:52 +0500

    created chat room repository and it's crud service

---
+++ src/Chat.Application/Interfaces/IChatRoomService.cs
@@ -0,0 +1,14 @@
+﻿using Chat.Domain.Entities;
+
+namespace Chat.Application.Interfaces;
+public interface IChatRoomService
+{
+    IQueryable<ChatRoom> Get(bool asNoTracking = true);
+
+    ValueTask<ChatRoom?> GetByIdAsync(Guid id, bool asNoTracking = true, CancellationToken cancellationToken = default);
+
+    ValueTask<ChatRoom> CreateAsync(ChatRoom chatRoom, bool saveChanges = true, CancellationToken cancellationToken = default);
+
+    ValueTask<ChatRoom> UpdateAsync(ChatRoom chatRoom, bool saveChanges = true, CancellationToken cancellationToken = default);
+    ValueTask<ChatRoom> DeleteAsync(ChatRoom chatRoom, bool saveChanges = true, CancellationToken cancellationToken = default);
+}
---
+++ src/Chat.Infrastructure/Services/ChatRoomService.cs
@@ -0,0 +1,32 @@
+﻿using Chat.Application.Interfaces;
+using Chat.Domain.Entities;
+using Chat.Persistence.Repositories.Interfaces;
+
+namespace Chat.Infrastructure.Services;
+public class ChatRoomService(IChatRoomRepository repository) : IChatRoomService
+{
+    public IQueryable<ChatRoom> Get(bool asNoTracking = true)
+    {
+        return repository.Get(asNoTracking);
+    }
+
+    public ValueTask<ChatRoom?> GetByIdAsync(Guid id, bool asNoTracking = true, CancellationToken cancellationToken = default)
+    {
+        return repository.GetByIdAsync(id, asNoTracking, cancellationToken);
+    }
+
+    public ValueTask<ChatRoom> CreateAsync(ChatRoom chatRoom, bool saveChanges, CancellationToken cancellationToken)
+    {
+        return repository.CreateAsync(chatRoom, saveChanges, cancellationToken);
+    }
+
+    public ValueTask<ChatRoom> UpdateAsync(ChatRoom chatRoom, bool saveChanges = true, CancellationToken cancellationToken = default)
+    {
+        return repository.UpdateAsync(chatRoom, saveChanges, cancellationToken);
+    }
+
+    public ValueTask<ChatRoom> DeleteAsync(ChatRoom chatRoom, bool saveChanges, CancellationToken cancellationToken)
+    {
+        return repository.DeleteAsync(chatRoom, saveChanges, cancellationToken);
+    }
+}
